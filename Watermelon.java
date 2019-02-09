5e25d31de981e272d2e8cbe21ce6721512e0c70f
Amr Mohamed <47480508+amr4mk-g@users.noreply.github.com>
2019-02-09 19:19:52 +0200

    http://codeforces.com/contest/4/problem/A

---
+++ Watermelon.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class watermelon{
+public static void main(String[] args){
+Scanner in = new Scanner(System.in);
+int x = in.nextInt();
+if((x-2)!=0&&(x-2)%2==0)
+{System.out.println("YES");}
+else {System.out.println("NO");} 
+}}
