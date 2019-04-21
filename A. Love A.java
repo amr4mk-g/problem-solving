09cbbdedc72a74d47087f657cc9db78bffa0f01c
Amr Mohamed <47480508+amr4mk-g@users.noreply.github.com>
2019-04-21 17:32:49 +0200

    http://codeforces.com/contest/1146/problem/A

---
+++ A. Love A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Love{
+public static void main(String[] args){
+Scanner in=new Scanner(System.in);
+String x=in.nextLine();
+int u=0;
+for(int i=0;i<x.length();i++)
+{if(x.charAt(i)=='a'){u++;}}
+int y=0;
+if(u>(x.length()/2))
+{y=x.length();}
+else{if(u==1){y=1;}
+else{y=(u*2)-1;}}
+System.out.print(y);
+}}
